b252c8e5cf63896a375b39e5b473c3fc730c4567
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2025-06-28 21:42:31 +0530

    middleware logic added

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-import express from "express";
+import express, { urlencoded } from "express";
 
 import { PORT } from "./config/env.js";
 import authRouter from "./routes/auth.Routes.js";
 import userRouter from "./routes/user.routes.js";
 import subRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/monjodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 
 const app = express()
 
+app.use(express.json())
+app.use(urlencoded({extended: false}));
+app.use(cookieParser);
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subRouter)
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res)=>{
     res.send("Helloo world");
