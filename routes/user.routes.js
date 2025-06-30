40933f8702ec1d045dccc82c475484703b87f17c
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2025-06-30 15:17:09 +0530

    user controller added

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { fetchUser, fetchUsers } from "../controllers/users.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title:"GET all users"}));
-userRouter.get('/:id', (req, res) => res.send({title: "Get a user"}));
+userRouter.get('/', fetchUsers);
+userRouter.get('/:id', authorize,fetchUser);
 userRouter.post('/', (req, res) => res.send({title: "Create user"}));
 userRouter.put('/:id', (req, res) => res.send({title: "Update currnet user"}));
 userRouter.delete('/:id', (req, res) => res.send({title: "Delete a user"}));
